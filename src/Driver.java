6e099fd1eb14908c26844dddaeef0be414588a4e
Bartholomew Tran <tranbartholomew@gmail.com>
2025-10-08 14:47:08 -0700

    writing comments, need to do output.

--- src/Driver.java
+++ src/Driver.java
@@ -6,21 +6,64 @@ Assignment 2a
 Personal Information Class
 
 1. DESIGN
+
+The objective of this project is to create a database to hold people's identifying information.
+It is designed for us to practice with classes.
+For this particular exercise, we will limit the database size to 3 individuals, codenamed alpha,
+bravo, and charlie.
+
+Each person will have their data inputted through the scanner.
+There are two classes: Driver and Person. Driver will be what calls the methods in Person to
+input, store, and display data.
+
+Driver has 2 methods: main and readValue. Main provides the commands on how the program is to run.
+readValue is in charge of recording data that will be collected in Person class and then displayed
+through a method in Person.
+
+
+2. VARIABLES pt1
+
+The main variables here are the identifiable information:
+
+    string name
+    string address
+    int age
+    string phone_number
+
+These are designed and then implemented in Person class.
+
 */
 
+/*
+
+3. PROCESSING pt1
+
+This is the processing of the main method in the Driver class. It's telling the program
+to perform the same tasks as outlined in readValue and Person per person.
+
+ */
 public class Driver {
     static Scanner s1 = new Scanner(System.in);
     public static void main(String [] args)
     {
         Driver d1 = new Driver();
         // test cmd
-        Person Stacey = d1.readValue();
-        Stacey.print();
-        Person Ross = d1.readValue();
-        Ross.print();
-        Person Mia = d1.readValue();
-        Mia.print();
+        Person alpha = d1.readValue();
+        alpha.print();
+        Person bravo = d1.readValue();
+        bravo.print();
+        Person charlie = d1.readValue();
+        charlie.print();
     }
+
+/*
+
+4. INPUT
+
+The input is the scanner that will allow the user to input identifiable data in a
+sequence.
+
+ */
     public Person readValue() {
         //use methods in Scanner to read values - create a Person object and return it.
         Scanner input = new Scanner(System.in);
@@ -40,42 +83,12 @@ public class Driver {
 }
 
 /*
-Design
-Variables, Input, Processing and Output.
 
+5. PRINT
 
-Submit a class diagram, test runs and code (.java file) with your submission.
-Please create a zip file and submit a single attachment for part 1.
-
-
-
-
-class definition (blueprint) --> object
-
-class anatomy (internal structure)
- --instance variables
- --static variables
- --constructors
- --instance methods
- ----getters/setters
- ----business methods (calculations)
- ----print()
- --static methods - usually update static variables [will not be used to update values of
-instance variables] -- because static methods can be used without creating an object.
-
-
-        **********************************************************
-Java coding standards
-writing a class -
-Class name should start with an upper case letter.
-class should always be public
-Must include a default constructor.
-All instance variables (properties) must be private
-All methods must be public.
-Class anatomy - instance variables, static variables, constructors, getter and setters, instance methods, static methods.
-Driver (class with a main method) should be in its own class.
-Every class must have a print method that can print the values of instance variables.
-One class per .java file
-**********************************************************\
+The data is printed through calling the public void print() method, which is part of the Person class.
+Check notes in Person. The method will specify which data is needed to be called, so in this case, it
+will be the name, address, age, and phone number data.
 
  */
+
--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,30 @@
 
+/*
+
+1. VARIABLES pt2
+
+These are the instance variables to be used in Person class. They are the same as the variables described in
+Driver class. However, through the use of this constructor, we are making the instance variables private.
+The reason is so that only this constructor (public class Person) has control over these instance variables.
+Therefore, data inputted from readValue from Driver class will be brought to this class for processing.
+
+ */
 // to create
 public class Person {
     private String name;
     private String address;
     private int age;
     private String phone_number;
+
+/*
+
+2. PROCESSING pt2
+
+This is where the data is processed. The data will be cached into the instance variables through the
+this.<variable> keyword. this.<variable> ensures that the specific instance variable is where the data
+will be cached. After reassigning, the method will be called again in Driver class.
+
+ */
 //overloaded (to upload)
     public Person(String name, String address, int age, String phone_number) { //watch out for order
         this.name = name;
@@ -48,7 +68,13 @@ public class Person {
     public void setPhone_number(String phone_number) {
         this.phone_number = phone_number;
     }
+/*
 
+5. PRINT
+
+The data is printed in this method. This method is called in the Driver class.
+
+ */
     public void print() {
         System.out.printf("---------------------------------------------\n" +
                 "This is the data of the person you entered:\n\n");
